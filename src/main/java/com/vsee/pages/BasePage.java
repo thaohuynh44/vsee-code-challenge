26395c904420fc6eefb2f03f8b9080ff83b0970c
thaohuynh <huynhngocthao989@gmail.com>
2025-06-09 21:25:56 +0700

    Add more methods in BasePage and update the access modifier to protected

--- src/main/java/com/vsee/pages/BasePage.java
+++ src/main/java/com/vsee/pages/BasePage.java
@@ -1,13 +1,13 @@
 package com.vsee.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class BasePage {
 
@@ -20,19 +20,56 @@ public abstract class BasePage {
         PageFactory.initElements(driver, this);
     }
 
-    public abstract boolean isLoaded();
+    protected abstract boolean isLoaded();
 
     public void goTo(String url) {
         this.driver.get(url);
     }
 
-    public void waitAndInputText(WebElement element, String text) {
-        this.wait.until(ExpectedConditions.visibilityOf(element))
-                .sendKeys(text);
+    protected void waitAndInputText(WebElement element, String text, Keys... extraKeys) {
+        WebElement visibleElement = this.wait.until(ExpectedConditions.visibilityOf(element));
+        visibleElement.clear();
+        visibleElement.sendKeys(text);
+
+        if (extraKeys != null && extraKeys.length > 0) {
+            for (Keys key : extraKeys) {
+                visibleElement.sendKeys(key);
+            }
+        }
     }
 
-    public void waitAndClick(WebElement element) {
+    protected void waitAndClick(WebElement element) {
         this.wait.until(ExpectedConditions.visibilityOf(element))
                 .click();
+    }
+
+    protected boolean waitUntilAnyElementVisible(List<WebElement> elements, int timeoutInSec) {
+        WebDriverWait _wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSec));
+        try {
+            return _wait.until(driver -> {
+                for(WebElement element: elements) {
+                    try {
+                        if(element.isDisplayed()) {
+                            return true;
+                        }
+                    } catch (Exception ignored) {}
+                    return false;
+                }
+                return false;
+            });
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    protected void waitAndSwitchToFrame(WebElement iframeElement) {
+        this.wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(iframeElement))
+                .switchTo().frame(iframeElement);
+    }
+
+    protected List<String> getInnerTexts(List<WebElement> elements) {
+        return this.wait.until(ExpectedConditions.visibilityOfAllElements(elements))
+                .stream().map(WebElement::getText)
+                .collect(Collectors.toList());
     }
 }
