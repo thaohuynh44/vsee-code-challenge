5745447e289c0f61921b7920a1783bada1cf466c
thaohuynh <huynhngocthao989@gmail.com>
2025-06-10 15:02:41 +0700

    Update baseTest

--- src/test/java/com/vsee/tests/BaseTest.java
+++ src/test/java/com/vsee/tests/BaseTest.java
@@ -1,21 +1,64 @@
 package com.vsee.tests;
 
+import com.vsee.utils.Config;
+import com.vsee.utils.Constants;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BaseTest {
 
+    private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
     protected WebDriver driver;
 
-    @BeforeTest
-    public void setDriver() {
+    @BeforeSuite
+    public void setupConfig() {
+        Config.initialize();
+    }
 
+    @BeforeTest
+    public void setDriver() throws MalformedURLException {
+        this.driver = Boolean.parseBoolean(Config.get(Constants.GRID_ENABLED)) ? getRemoteDriver() : getLocalDriver();
+        driver.manage().window().maximize();
+    }
+
+    private WebDriver getLocalDriver() {
+        ChromeOptions options = getChromeOptions();
+
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver(options);
+    }
+
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        Capabilities capabilities = getChromeOptions();
+        if(Constants.FIREFOX.equalsIgnoreCase(Config.get(Constants.BROWSER))) {
+            capabilities = new FirefoxOptions();
+        }
+
+        String urlFormat = Config.get(Constants.GRID_URL_FORMAT);
+        String hubHost = Config.get(Constants.GRID_HUB_HOST);
+        String url = String.format(urlFormat, hubHost);
+        log.info("Grid URL: {}", url);
+
+        return new RemoteWebDriver(new URL(url), capabilities);
+    }
+
+    protected ChromeOptions getChromeOptions() {
         Map<String, Object> prefs = new HashMap<>();
         prefs.put("profile.default_content_setting_values.media_stream_camera", 1);
         prefs.put("profile.default_content_setting_values.media_stream_mic", 1);
@@ -25,8 +68,11 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("prefs", prefs);
 
-        WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
+        return options;
+    }
+
+    @AfterTest
+    public void tearDown() {
+        this.driver.quit();
     }
 }
