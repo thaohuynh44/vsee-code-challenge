bc86d676ad0720db3e6d422522c0cbfa3288b271
thaohuynh <huynhngocthao989@gmail.com>
2025-06-10 19:17:58 +0700

    Implement config reader

---
+++ src/main/java/com/vsee/utils/Constants.java
@@ -0,0 +1,20 @@
+package com.vsee.utils;
+
+public class Constants {
+
+    public static final String BROWSER = "browser";
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String DRIVER = "driver";
+
+    public static final String VISITOR_ROOM_URL = "visitorRoom.url";
+    public static final String PROVIDER_LOGIN_URL = "providerLogin.url";
+    public static final String PROVIDER_DASHBOARD_URL = "providerDashboard.url";
+
+    public static final String DEFAULT_PROVIDER_USR = "providerEmail";
+    public static final String DEFAULT_PROVIDER_PWD = "providerPassword";
+
+    public static final String GRID_ENABLED = "selenium.grid.enabled";
+    public static final String GRID_URL_FORMAT = "selenium.grid.urlFormat";
+    public static final String GRID_HUB_HOST = "selenium.grid.hubHost";
+}
